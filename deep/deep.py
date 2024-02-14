fddc4488f3703709735245bb44b033a227ef1a18
Sevann Radhak Triztan <sevann.radhak@gmail.com>
2024-02-14 20:47:49 -0300

    deep

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,8 @@
 def main():
-    goal = ['42', 'forty-two', 'forty two']    
+    goal = ['42', 'forty-two', 'forty two']   
+    input_user = input('What is the answer to the Great Question of Life, the Universe and Everything?: ') 
     
-    if contains_goal(goal, 'Hello, forrty   TwooooO World!'):
+    if contains_goal(goal, input_user):
         print('YES')
     else:
         print('NO')
