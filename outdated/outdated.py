2303469943a270b7e1fbd72e5d0a917b5c9a46cd
Sevann Radhak Triztan <sevann.radhak@gmail.com>
2024-02-15 23:20:47 -0300

    outdated

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,40 +22,38 @@ def process():
     while True:
         try:
             input_date = input('Date: ')
-            parts = input_date.strip().split(' ')
-
-            if len(parts) != 3:
-                parts = input_date.split('/')
-                if(len(parts) != 3):
-                    continue
-
-            day = int(parts[1])
-            month = parts[0]
-            year = int(parts[2])
-
-            if day > 31:
-                continue
-
-            if year < 0:
-                continue
-
+            input_date = input_date.strip()
+            input_date_parts_1 = input_date.split(' ')
+            input_date_parts_2 = input_date.split('/')
             month_number = 0
-
-            if month in M:
-                month_number = M.index(month) + 1
-            elif month.isnumeric():
-                month_number = int(month)
-
-                if month_number < 1 or month_number > 12:
+            
+            if len(input_date_parts_1) == 3:
+                month = input_date_parts_1[0]
+                day = input_date_parts_1[1]
+                year = int(input_date_parts_1[2])
+                
+                if month not in M:
                     continue
+                if not day.endswith(','):
+                    continue
+                
+                day = day[:-1]
+                day = int(day)   
+                
+                month_number = M.index(month) + 1            
+            elif len(input_date_parts_2) == 3:                
+                month_number = int(input_date_parts_2[0])
+                day = int(input_date_parts_2[1])
+                year = int(input_date_parts_2[2])
             else:
                 continue
-
+            
+            if day > 31 or month_number < 1 or month_number > 12 or year < 0:
+                continue
+            
             return f'{year:04}-{month_number:02}-{day:02}'
         except (EOFError, ValueError, IndexError, TypeError, AttributeError, NameError):
-            break
-
-    return
+            continue
 
 
 if __name__ == '__main__':
